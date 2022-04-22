bc7aa869183260f32c41c4816afc0e28a8457625
Oscar Rieken <oriekenjr@gmail.com>
2022-04-22 09:03:34 -0400

    lint(users): deleted extra spaces

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex justify-center gap-4">
     <ul class="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
-      <li 
-          class="px-6 py-2 borderb border-gray-200 w-full"
-          v-for="user in users"
-          :key="user.id"
-          :id="user.id"
+      <li
+        class="px-6 py-2 borderb border-gray-200 w-full"
+        v-for="user in users"
+        :key="user.id"
+        :id="user.id"
       >
         <div class="space-x-4">
           <img
